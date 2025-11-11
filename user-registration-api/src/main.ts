0512a3577ba50d6de54e0c61b157f227e592e69b
ngocnhu100 <144573999+ngocnhu100@users.noreply.github.com>
2025-11-11 22:36:29 +0700

    fix: move main.ts to src

--- user-registration-api/src/main.ts
+++ user-registration-api/src/main.ts
@@ -1,7 +1,7 @@
 import "reflect-metadata";
 import "dotenv/config";
 import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
+import { AppModule } from "./app/app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
